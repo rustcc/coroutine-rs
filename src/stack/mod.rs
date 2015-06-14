468ae081cd13dfc9007adadc13455abc9c8998c9
Y. T. Chung <zonyitoo@gmail.com>
2015-06-14 08:46:25 +0800

    add standard allocate stack for systems that are not unix and windows

---
+++ src/stack/mod.rs
@@ -0,0 +1,38 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Rustcc Developers
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+pub use self::stack_pool::StackPool;
+
+#[cfg(any(unix, windows))]
+pub use self::stack_protected::Stack;
+
+#[cfg(not(any(unix, windows)))]
+pub use self::stack_standard::Stack;
+
+pub mod stack_pool;
+
+#[cfg(any(unix, windows))]
+pub mod stack_protected;
+
+#[cfg(not(any(unix, windows)))]
+pub mod stack_standard;
+
--- src/stack/stack_protected.rs
+++ src/stack/stack_protected.rs
@@ -1,16 +1,27 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Rustcc Developers
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
 
 use std::ptr;
-use std::sync::atomic;
-use std::env::{self, page_size};
+use std::env::page_size;
 use std::fmt;
 
 use libc;
@@ -109,6 +120,12 @@ impl Stack {
             })
             .unwrap_or(ptr::null())
     }
+
+    /// Minimun size of this stack
+    #[inline(always)]
+    pub fn min_size(&self) -> usize {
+        self.min_size
+    }
 }
 
 #[cfg(unix)]
@@ -132,78 +149,5 @@ fn protect_last_page(stack: &MemoryMap) -> bool {
         libc::VirtualProtect(last_page, page_size() as libc::SIZE_T,
                              libc::PAGE_NOACCESS,
                              &mut old_prot as libc::LPDWORD) != 0
-    }
-}
-
-#[derive(Debug)]
-pub struct StackPool {
-    // Ideally this would be some data structure that preserved ordering on
-    // Stack.min_size.
-    stacks: Vec<Stack>,
-}
-
-impl StackPool {
-    pub fn new() -> StackPool {
-        StackPool {
-            stacks: vec![],
-        }
-    }
-
-    pub fn take_stack(&mut self, min_size: usize) -> Stack {
-        // Ideally this would be a binary search
-        match self.stacks.iter().position(|s| min_size <= s.min_size) {
-            Some(idx) => self.stacks.swap_remove(idx),
-            None => Stack::new(min_size)
-        }
-    }
-
-    pub fn give_stack(&mut self, stack: Stack) {
-        if self.stacks.len() <= max_cached_stacks() {
-            self.stacks.push(stack)
-        }
-    }
-}
-
-fn max_cached_stacks() -> usize {
-    static mut AMT: atomic::AtomicUsize = atomic::ATOMIC_USIZE_INIT;
-    match unsafe { AMT.load(atomic::Ordering::SeqCst) } {
-        0 => {}
-        n => return n - 1,
-    }
-    let amt = env::var("RUST_MAX_CACHED_STACKS").ok().and_then(|s| s.parse().ok());
-    // This default corresponds to 20M of cache per scheduler (at the
-    // default size).
-    let amt = amt.unwrap_or(10);
-    // 0 is our sentinel value, so ensure that we'll never see 0 after
-    // initialization has run
-    unsafe { AMT.store(amt + 1, atomic::Ordering::SeqCst); }
-    return amt;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::StackPool;
-
-    #[test]
-    fn stack_pool_caches() {
-        let mut p = StackPool::new();
-        let s = p.take_stack(10);
-        p.give_stack(s);
-        let s = p.take_stack(4);
-        assert_eq!(s.min_size, 10);
-        p.give_stack(s);
-        let s = p.take_stack(14);
-        assert_eq!(s.min_size, 14);
-        p.give_stack(s);
-    }
-
-    #[test]
-    fn stack_pool_caches_exact() {
-        let mut p = StackPool::new();
-        let s = p.take_stack(10);
-        p.give_stack(s);
-
-        let s = p.take_stack(10);
-        assert_eq!(s.min_size, 10);
     }
 }
